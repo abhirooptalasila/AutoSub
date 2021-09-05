5c4a6a3fa6054e1b627c5ee2f13915de2b3b71c2
Seth Falco <seth@falco.fun>
2021-09-05 20:59:06 +0200

    chores: editorconfig and dockerfile

--- autosub/segmentAudio.py
+++ autosub/segmentAudio.py
@@ -12,7 +12,7 @@ import trainAudio as TA
 def read_audio_file(input_file):
     """This function returns a numpy array that stores the audio samples of a
     specified WAV file
-    
+
     Args:
         input_file : audio from input video file
     """
@@ -53,12 +53,12 @@ def smooth_moving_avg(signal, window=11):
               signal, 2 * signal[-1] - signal[-1:-window:-1]]
     w = np.ones(window, 'd')
     y = np.convolve(w/w.sum(), s, mode='same')
-    
+
     return y[window:-window + 1]
 
 def stereo_to_mono(signal):
     """This function converts the input signal to MONO (if it is STEREO)
-    
+
     Args:
         signal: audio file stored in a Numpy array
     """
@@ -69,13 +69,13 @@ def stereo_to_mono(signal):
         else:
             if signal.shape[1] == 2:
                 signal = (signal[:, 1] / 2) + (signal[:, 0] / 2)
-                
+
     return signal
 
 def silence_removal(signal, sampling_rate, st_win, st_step, smooth_window=0.5,
                     weight=0.5):
     """Event Detection (silence removal)
-    
+
     Args:
         signal : the input audio signal
         sampling_rate : sampling freq
@@ -83,10 +83,10 @@ def silence_removal(signal, sampling_rate, st_win, st_step, smooth_window=0.5,
         smoothWindow : (optinal) smooth window (in seconds)
         weight : (optinal) weight factor (0 < weight < 1) the higher, the more strict
         plot : (optinal) True if results are to be plotted
-        
+
     Returns:
-        seg_limits : list of segment limits in seconds (e.g [[0.1, 0.9], 
-                [1.4, 3.0]] means that the resulting segments 
+        seg_limits : list of segment limits in seconds (e.g [[0.1, 0.9],
+                [1.4, 3.0]] means that the resulting segments
                 are (0.1 - 0.9) seconds and (1.4, 3.0) seconds
     """
 
@@ -189,13 +189,13 @@ def silenceRemoval(input_file, smoothing_window = 1.0, weight = 0.2):
         smoothing : Smoothing window size in seconds. Defaults to 1.0.
         weight : Weight factor in (0,1). Defaults to 0.5.
     """
-    
+
     if not os.path.isfile(input_file):
         raise Exception("Input audio file not found!")
 
     [fs, x] = read_audio_file(input_file)
     segmentLimits = silence_removal(x, fs, 0.05, 0.05, smoothing_window, weight)
-    
+
     for i, s in enumerate(segmentLimits):
         strOut = "{0:s}_{1:.3f}-{2:.3f}.wav".format(input_file[0:-4], s[0], s[1])
         wavfile.write(strOut, fs, x[int(fs * s[0]):int(fs * s[1])])
--- autosub/writeToFile.py
+++ autosub/writeToFile.py
@@ -11,32 +11,32 @@ def write_to_file(file_handle, inferred_text, line_count, limits, vtt, cues):
     Args:
         file_handle : SRT file handle
         inferred_text : text to be written
-        line_count : subtitle line count 
+        line_count : subtitle line count
         limits : starting and ending times for text
     """
 
     sep = '.' if vtt else ','
-    
+
     d = str(datetime.timedelta(seconds=float(limits[0])))
     try:
         from_dur = "0" + str(d.split(".")[0]) + sep + str(d.split(".")[-1][:2])
     except:
         from_dur = "0" + str(d) + sep + "00"
-        
+
     d = str(datetime.timedelta(seconds=float(limits[1])))
     try:
         to_dur = "0" + str(d.split(".")[0]) + sep + str(d.split(".")[-1][:2])
     except:
         to_dur = "0" + str(d) + sep + "00"
 
-    if not vtt:    
+    if not vtt:
         file_handle.write(str(line_count) + "\n")
         file_handle.write(from_dur + " --> " + to_dur + "\n")
         file_handle.write(inferred_text + "\n\n")
     else:
         file_handle.write(from_dur + " --> " + to_dur + "  align:start position:0%\n")
         file_handle.write(inferred_text + "\n")
-        
+
         words = [f"<c> {x}</c>" for x in inferred_text.split(" ")]
         cue_tags = [f"<{str(datetime.timedelta(seconds=cue))}>" for cue in cues]
         words_cues_mixed = [val for pair in zip(cue_tags, words) for val in pair][1:]
